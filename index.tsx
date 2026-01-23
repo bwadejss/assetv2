800e0588de845f0039b68b842f585864a40d6673
bwadejss <bwadejs@googlemail.com>
2026-01-23 22:17:42 +0000

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,16 +1,16 @@
 
+// This file is handled via the script tag in index.html for direct transpilation.
+// Keep this file here so that local development environments still recognize the entry point.
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
