419482c42afaa847934a4b88f77f21b225e3f5ba
bwadejss <bwadejs@googlemail.com>
2026-01-23 22:41:55 +0000

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,16 +1,1 @@
-
-// This file is handled via the script tag in index.html for direct transpilation.
-// Keep this file here so that local development environments still recognize the entry point.
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+// Entry point is handled by the script tag in index.html to avoid CORB issues.
